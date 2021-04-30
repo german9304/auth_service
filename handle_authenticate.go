1776c60f990c599f41f734792cf1007d3a067cfe
German Razo Rodriguez <ger9366@gmail.com>
2021-04-29 20:52:47 -0700

    Handler authentication
    Add query parameters

--- handle_authenticate.go
+++ handle_authenticate.go
@@ -1,9 +1,29 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+)
+
+type authQueryMeta struct {
+	responseType string
+	redirectUri  string
+	clientId     string
+	responseMode string
+}
+
+func (r authQueryMeta) new(queryValues url.Values) authQueryMeta {
+	return authQueryMeta{
+		responseType: queryValues.Get("response_type"),
+		redirectUri:  queryValues.Get("redirect_uri"),
+		clientId:     queryValues.Get("client_id"),
+		responseMode: queryValues.Get("response_mode"),
+	}
+}
 
 // handles authentication
 func handleAuthenticate() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
 	}
 }
